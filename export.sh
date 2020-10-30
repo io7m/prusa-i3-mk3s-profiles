57d7fadaa998c96e3a2fe4040230b6d2d0d91fa3
Mark Raynsford <code@io7m.com>
2020-10-30 18:49:06 +0000

    Add Eryone PETG Fan15 variant

--- export.sh
+++ export.sh
@@ -12,7 +12,10 @@ rm -rfv "${PROFILES}"
 mkdir -p "${FILAMENT}"
 mkdir -p "${PRINT}"
 
-${EXPORT} "filament/PETG_Eryone_2020-10-26.ini" > "${FILAMENT}/PETG_Eryone_2020-10-26.ini"
+${EXPORT} "filament/PETG_Eryone_2020-10-26.ini"         > "${FILAMENT}/PETG_Eryone_2020-10-26.ini"
+${EXPORT} "filament/PETG_Eryone_2020-10-26_Fan15.ini"   > "${FILAMENT}/PETG_Eryone_2020-10-26_Fan15.ini"
+${EXPORT} "filament/PETG_Eryone_2020-10-26_Fanless.ini" > "${FILAMENT}/PETG_Eryone_2020-10-26_Fanless.ini"
+
 ${EXPORT} "print/LH0.3_N0.4_FastPLA.ini"        > "${PRINT}/LH0.3_N0.4_FastPLA.ini"
 ${EXPORT} "print/LH0.3_N0.4_PETG.ini"           > "${PRINT}/LH0.3_N0.4_PETG.ini"
 ${EXPORT} "print/LH0.3_N0.4_PETGStrong.ini"     > "${PRINT}/LH0.3_N0.4_PETGStrong.ini"
