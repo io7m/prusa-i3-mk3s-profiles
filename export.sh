0bd1d67495f77ae3697c70a3515836117e8a6011
Mark Raynsford <code@io7m.com>
2020-10-31 18:19:35 +0000

    Correct export.sh

--- export.sh
+++ export.sh
@@ -13,7 +13,6 @@ mkdir -p "${FILAMENT}"
 mkdir -p "${PRINT}"
 
 ${EXPORT} "filament/PETG_Eryone_2020-10-26.ini"         > "${FILAMENT}/PETG_Eryone_2020-10-26.ini"
-${EXPORT} "filament/PETG_Eryone_2020-10-26_Fan15.ini"   > "${FILAMENT}/PETG_Eryone_2020-10-26_Fan15.ini"
 ${EXPORT} "filament/PETG_Eryone_2020-10-26_Fanless.ini" > "${FILAMENT}/PETG_Eryone_2020-10-26_Fanless.ini"
 
 ${EXPORT} "print/LH0.3_N0.4_FastPLA.ini"        > "${PRINT}/LH0.3_N0.4_FastPLA.ini"
