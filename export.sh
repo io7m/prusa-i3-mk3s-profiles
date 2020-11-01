da8e7e1939c398d720d657e08e37fb10825f18e5
Mark Raynsford <code@io7m.com>
2020-11-01 12:47:11 +0000

    Export base profiles

--- export.sh
+++ export.sh
@@ -17,8 +17,12 @@ ${EXPORT} "filament/PETG_Eryone_2020-10-26_Fanless.ini" > "${FILAMENT}/PETG_Eryo
 ${EXPORT} "filament/PETG_Eryone_2020-10-26_Fan5.ini"    > "${FILAMENT}/PETG_Eryone_2020-10-26_Fan5.ini"
 ${EXPORT} "filament/ABS_eSun_2020-11-01.ini"            > "${FILAMENT}/ABS_eSun_2020-11-01.ini"
 
+${EXPORT} "print/LH0.3_N0.4_Base.ini"           > "${PRINT}/LH0.3_N0.4_Base.ini"
 ${EXPORT} "print/LH0.3_N0.4_FastPLA.ini"        > "${PRINT}/LH0.3_N0.4_FastPLA.ini"
 ${EXPORT} "print/LH0.3_N0.4_PETG.ini"           > "${PRINT}/LH0.3_N0.4_PETG.ini"
 ${EXPORT} "print/LH0.3_N0.4_PETGStrong.ini"     > "${PRINT}/LH0.3_N0.4_PETGStrong.ini"
+
+${EXPORT} "print/LH0.2_N0.4_Base.ini"           > "${PRINT}/LH0.2_N0.4_Base.ini"
 ${EXPORT} "print/LH0.2_N0.4_PETG.ini"           > "${PRINT}/LH0.2_N0.4_PETG.ini"
 ${EXPORT} "print/LH0.2_N0.4_PETGStrong.ini"     > "${PRINT}/LH0.2_N0.4_PETGStrong.ini"
+
